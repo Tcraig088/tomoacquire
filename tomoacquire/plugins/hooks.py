1e7c54f91c4ef112fed1899755b21f5aa4a4d709
Timothy <tcraig258@gmail.com>
2024-10-09 15:43:44 +0200

    Update tomoacquire submodule reference

--- tomoacquire/plugins/hooks.py
+++
@@ -1,9 +0,0 @@
-
-
-
-def tomoacquire_hook_tiltscheme(name):
-    def decorator(cls):
-        cls.tomoacquire_name = name
-        cls.is_tomoacquire_tiltscheme = True
-        return cls
-    return decorator
--- tomoacquire/plugins/tiltschemes/__init__.py
+++
@@ -1,3 +0,0 @@
-from tomoacquire.plugins.tiltschemes.incremental import Incremental
-from tomoacquire.plugins.tiltschemes.grs import GRS
-from tomoacquire.plugins.tiltschemes.binary import Binary
