a3744e86081d6d8ef172e94f98258dbdada0427b
Timothy <tcraig258@gmail.com>
2025-03-27 20:13:11 +0100

    implement threading for beam blanking and acquisition updates in FEIMicroscope - Fixes

--- tomoacquire/microscopes/FEI.py
+++ tomoacquire/microscopes/FEI.py
@@ -1,3 +1,5 @@
+from threading import Thread, Lock
+import time
 from tomoacquire.hooks import tomoacquire_hook
 from tomobase.data import Sinogram, Image
 import numpy as np
@@ -10,11 +12,12 @@ import enum
 import temscript
 from tomoacquire.states import ImagingState
 from qtpy.QtCore import Signal, QObject
-#import pythoncom
+from tomobase.log import logger
 
 @tomoacquire_hook(name="FEI")
 class FEIMicroscope(QObject):
     scanwindow_updated = Signal()
+    acqwindow_updated = Signal()
 
     def __init__(self, address, port, magnifications, detectors, detector_pixelsize):
         super().__init__()
@@ -42,17 +45,44 @@ class FEIMicroscope(QObject):
         else:
             self.microscope = temscript.RemoteMicroscope((self.address, str(self.port)))
 
+
     @property
     def isblank(self):
         return self._isblank
     
     @isblank.setter
     def isblank(self, value):
-        self._isblank = value
-        self.microscope.set_beam_blanked(value)
+        self.queue_thread(self.blankbeam, value)
 
-    def set_magnification(self, index):
-        if type(mag) == int:
+    def blankbeam(self, blank):
+        self._isblank = blank
+        self.microscope.set_beam_blanked(blank)
+
+    def queue_thread(self, func, *args, **kwargs):
+        if self.state == ImagingState.Idle:
+            self.state = ImagingState.Queued
+            acquire_thread = Thread(target=self.exec_thread, args=(func, *args), kwargs=kwargs)
+            acquire_thread.daemon = True
+            acquire_thread.start()
+
+        else:
+            logger.info("cannot queue more than 1 command please wait for execution")
+
+    def exec_thread(self, func, *args, **kwargs):
+        while self.state == ImagingState.Queued or self.state == ImagingState.Idle:
+            time.sleep(0.05)
+        
+        func(*args, **kwargs)
+        self.state = ImagingState.Idle
+        if self._isscaninit:
+            self.start_scan()
+
+
+    def set_scan_mode(self, scanning=True):
+        self.queue_thread(self.setscanmode, scanning)
+
+    def set_magnification(self, mag):
+        if isinstance(mag, int):
             mag = self.magnification_options[mag]
             self.microscope.set_stem_magnification(mag)
 
@@ -89,12 +119,17 @@ class FEIMicroscope(QObject):
         self.microscope.set_stem_acquisition_param(_dict)
 
     def start_scan(self):
-        data = np.zeros((len(self._scan_detectors), self._scan_frame, self._scan_frame))
-        self.scanwindow = Image(data, pixelsize=1.0)
-        self.setscanmode(scanning=True)
-        self._isscaninit = True
-        
-        acquire_thread = Thread(target=self.acquire)
+        if not self._isscaninit:
+            data = np.zeros((len(self._scan_detectors), self._scan_frame, self._scan_frame))
+            self.scanwindow = Image(data, pixelsize=1.0)
+            self.setscanmode(scanning=True)
+            self._isscaninit = True
+
+            data = np.zeros((len(self._acquire_detectors), self._acquire_frame, self._acquire_frame))
+            self.acqwindow = Image(data, pixelsize=1.0)
+
+
+        acquire_thread = Thread(target=self.scan)
         acquire_thread.daemon = True
         acquire_thread.start()
 
@@ -104,15 +139,38 @@ class FEIMicroscope(QObject):
             self.scanwindow.data[i,:,:] = value
         self.scanwindow_updated.emit() 
 
-    def acquire(self):
-         while self._isscaninit:
+    def update_acqwindow(self, new_data):
+        # Convert dictionary to numpy array
+        for i, (key, value) in enumerate(new_data.items()):
+            self.acqwindow.data[i,:,:] = value
+        self.acqwindow_updated.emit()
+
+
+    def scan(self):
+        scanning = True
+        logger.info("Starting acquisition...")
+        while scanning:
+            #with self._microscope_lock:  # Ensure thread-safe access
             _dict = self.microscope.acquire(*self.detectors)
+            logger.info("Current State: %s", self.state)
+            time.sleep(self.sleep)
+
             if self._isnull:
                 for i, item in enumerate(self.detectors):
                     _dict[item] = np.random.random([512, 512])
-            time.sleep(self.sleep)
+
             if self._isscan:
                 self.update_scanwindow(_dict)
+                logger.info("Scan window updated")
+            else:
+                self.update_acqwindow(_dict)
+                logger.info("Acquisition window updated")
+            
+            if self.state == ImagingState.Queued:
+                scanning = False
+
+        self.state = ImagingState.Executing
+        logger.info("Acquisition complete")
 
 
 
